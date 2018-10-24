b1c912f0c0dd3a96f163f6913401bd185bb2b691
DeftCorgi <toanau123@gmail.com>
2018-10-24 18:23:56 +1100

    collision resolution

--- static/scripts/collisions.js
+++ static/scripts/collisions.js
@@ -36,6 +36,55 @@ window.addEventListener('resize', e => {
   canvas.width = window.innerWidth;
 });
 
+// rotates our velocity based on an angle
+const rotate = (velocity, angle) => {
+  const rotatedVelocities = {
+    x: velocity.x * Math.cos(angle) - velocity.y * Math.sin(angle),
+    y: velocity.x * Math.sin(angle) + velocity.y * Math.cos(angle)
+  };
+
+  return rotatedVelocities;
+};
+
+// resolves the collision between 2 circles
+const resolveCollision = (circle1, circle2) => {
+  const xVelocityDiff = circle2.dx - circle1.dx;
+  const yVelocityDiff = circle2.dy - circle1.dy;
+
+  const xDistance = circle2.x - circle1.x;
+  const yDistance = circle2.y - circle1.y;
+
+  if (xVelocityDiff * xDistance - yVelocityDiff * yDistance >= 0) {
+    const m1 = circle1.mass;
+    const m2 = circle2.mass;
+
+    // arc tangent of
+    const angle = Math.atan2(circle1.x - circle2.x, circle1.y - circle2.y);
+
+    // velcotiy before equation
+    const u1 = rotate(circle1.velocity, angle);
+    const u2 = rotate(circle2.velocity, angle);
+
+    // velocity after 1D collision equation
+    const x1 = (u1 * (m1 - m2) + 2 * m2 * u2) / m1 + m2;
+    const x2 = (u2 * (m2 - m1) + 2 * m1 * u1) / m1 + m2;
+
+    const v1 = { x: x1, y: u1.y };
+    const v2 = { x: x2, y: u2.y };
+
+    // rotate back to atan proper angle
+    const finalV1 = rotate(v1, -angle);
+    const finalV2 = rotate(v2, -angle);
+
+    // swap velocitys
+    circle1.velocity.x = finalV1.x;
+    circle1.velocity.y = finalV1.y;
+
+    circle2.velocity.x = finalV2.x;
+    circle2.velocity.y = finalV2.y;
+  }
+};
+
 // caculate pythoagorean distance between 2 points
 const distance = (x1, x2, y1, y2) => {
   const xDist = x2 - x1;
@@ -55,6 +104,8 @@ class Circle {
     this.x = Math.random() * (innerWidth - this.radius * 2) + this.radius;
     this.y = Math.random() * (innerHeight - this.radius * 2) + this.radius;
     this.friction = 0.95;
+    this.velocity = { x: this.dx, y: this.dy };
+    this.mass = 1;
   }
 
   draw() {
@@ -67,7 +118,7 @@ class Circle {
     // c.fill();
   }
 
-  update() {
+  update(colliders) {
     // bounce at edges
     if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
       this.dx = -this.dx;
@@ -76,12 +127,18 @@ class Circle {
     if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
       this.dy = -this.dy * this.friction;
       this.dx = this.dx * 0.95;
-    } else {
-      // gravity
-      // this.dy += GRAVITY;
     }
 
-    // interactivity
+    // collision detection between circles
+    colliders.map(c => {
+      // if collider is not itself
+      if (this !== c) {
+        // if collision detected
+        if (distance(this.x, c.x, this.y, c.y) < CIRCLE_RADIUS * 2) {
+          resolveCollision(this, c);
+        }
+      }
+    });
 
     // update position based on velocity
     this.x += this.dx;
@@ -93,7 +150,7 @@ class Circle {
 
 // circles array
 let circles = [];
-init = () => {
+const init = () => {
   const initCircles = [new Circle()];
   for (let i = 0; i < NUM_CIRCLES - 1; i++) {
     let newCircle = new Circle();
@@ -124,7 +181,7 @@ animate = () => {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, innerWidth, innerHeight);
   // draw all circles
-  circles.map(circle => circle.update());
+  circles.map(circle => circle.update(circles));
 };
 
 init();
