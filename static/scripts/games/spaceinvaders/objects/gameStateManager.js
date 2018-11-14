1dafa5905b680b57083d41898b345b90e69b6736
DeftCorgi <toanau123@gmail.com>
2018-11-14 17:29:38 +1100

    paused functionality

--- static/scripts/games/spaceinvaders/objects/gameStateManager.js
+++ static/scripts/games/spaceinvaders/objects/gameStateManager.js
@@ -9,7 +9,23 @@ class GameStateManager {
     this.draw();
   }
 
-  draw() {}
+  draw() {
+    // display menu if paused
+    if (this.paused) {
+      this.drawMenu();
+    }
+  }
+
+  drawMenu() {
+    textAlign(CENTER, CENTER);
+    text('- Paused -', width / 2, height / 2);
+  }
 }
 
 var manager = new GameStateManager();
+
+function keyPressed() {
+  if (keyCode === 80 || keyCode === ESCAPE) {
+    manager.paused = !manager.paused;
+  }
+}
